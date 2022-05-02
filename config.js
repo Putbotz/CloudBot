f1703439654e2b1269fb5c23abfe233761a9393d
Putbotz <96361236+Putbotz@users.noreply.github.com>
2022-05-02 11:37:19 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['6281224863098'],
+  ['60147366955'],
   ['0'],
-  ['6281224863098', 'Della', true]
+  ['60147366955', 'Putbotz', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -31,7 +31,7 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.packname = 'Created By'
-global.author = 'Cloud Bot'
+global.author = 'Putbotz'
 
 global.multiplier = 69 // The higher, The harder levelup
 
